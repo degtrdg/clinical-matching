234852d00eb9af22ce7706747591d60563fcdbda
arnav-jain1 <116742881+arnav-jain1@users.noreply.github.com>
2023-11-04 23:27:02 -0500

    huggingface app.py done

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from CancerNET.VAEClass import open_file
 from CancerNET.VAEClass import VAE
 import numpy as np
 import pandas as pd
+from keras.utils import to_categorical
+
 
 params = open_file(r"./CancerNET/cancernet_hyperparams.pickle")
 
@@ -11,26 +13,39 @@ dxNet = VAE(
     params["encoder"],
     params["latent"],
     params["decoder"],
-    [100, 6],
+    [100, 34],
     params["loss_weights"],
 )
 
 dxNet.build_multimodel()
 
-dxNet.xnet.load_weights(r"./CancerNET/rare6class.h5")
+dxNet.xnet.load_weights(r"./CancerNET/34_class_CancerNet_weights.h5")
 
-inp = gr.DataFrame
-label = gr.outputs.Label()
+inp = gr.DataFrame()
+label = gr.Label()
+
 
 def test_inp(inp):
-    print(inp)
     inp = np.asarray(inp)
+    inp = inp.flatten()
+    inp = inp.reshape(1,-1)
     print(inp)
     output = dxNet.xnet.predict(inp)
     out = np.asarray(output[1])
-    return out
+    print(out)
+    predicted = make_categorical(out)
 
-inp = gr.dataFrame()
+    print(predicted)
+    result = {"output": predicted[0].tolist()}
+    return result
 
-interface = gr.Interface(fn=test_inp, inputs=inp, outputs=label)
+def make_categorical(y_score):
+
+    classes = y_score.shape[1]
+    y_classes = y_score.argmax(axis=1)
+    Y = to_categorical(y_classes, num_classes=classes)
+    return Y
+
+
+interface = gr.Interface(fn=test_inp, inputs=inp, outputs=gr.JSON())
 interface.launch(inline=False)
